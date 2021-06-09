788ec6c183da58b183e2506e443662a13d6914b3
Srihari <Srihari@zelarsoft.com>
2021-06-09 12:05:14 +0530

    update

---
+++ components/frontend.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+source components/common.sh
+
+#Used export instead of service file
+DOMAIN=ksrihari.online
+
+OS_PREREQ
+
+Head "Installing Nginx"
+apt install nginx -y &>>$LOG
+Stat $?
+
+Head "Starting Nginx Service"
+systemctl start nginx
+systemctl enable nginx
+Stat $?
+
+Head "Installing NPM"
+apt install npm -y &>>$LOG
+Stat $?
+
+Head "change and create the directories"
+cd /var/www/html
+rm -rf app
+mkdir app &>>$LOG
+cd app
+Stat $?
+
+DOWNLOAD_COMPONENT
+
+cd frontend
+
+Head "Installing NPM under the frontend path"
+npm install --save-dev  --unsafe-perm node-sass &>>$LOG
+Stat $?
+
+Head "Starting NPM"
+systemctl restart nginx
+npm start
+Stat $?
