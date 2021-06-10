335b7496d3f0bfdf97fd2d53b17cef0e4a404c52
Srihari <Srihari@zelarsoft.com>
2021-06-10 21:15:53 +0530

    update

--- components/login.sh
+++ components/login.sh
@@ -1,49 +1,31 @@
 #!/bin/bash
 
 source components/common.sh
-
-#Used export instead of service file
 DOMAIN=ksrihari.online
 
 OS_PREREQ
 
-Head " Adding User"
-deluser app
-useradd -m -s /bin/bash app &>>$LOG
+Head "Installing Golang"
+apt install golang -y &>>$LOG
+apt install unzip -y &>>$LOG
 Stat $?
 
-Head " Changing directory to app"
-cd /home/app/
-Stat $?
-
-Head "Run the following command as a user with sudo privileges to download and extract the Go binary archive in the /usr/local directory:"
-wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local &>>$LOG
-Stat $?
-
-Head "Adjusting the Path Variable"
-export PATH=$PATH:/usr/local/go/bin
-Stat $?
-
-Head "Save the file, and load the new PATH environment variable into the current shell session:"
-source ~/.profile
-Stat $?
-
-Head "Inside the workspace create a new directory /src"
-mkdir -p ~/go/src &>>$LOG
-cd  ~/go/src/
-Stat $?
-
-Head "Lets clone the code from github repository"
+Head "Downloading Component"
 DOWNLOAD_COMPONENT
-cd login/
-apt update
 Stat $?
 
-Head "Navigate** to the ~/go/src/login/login directory and run go build to build the program:"
-apt install go-dep &>>$LOG
-go get &>>$LOG
-go build &>>$LOG
+Head "Extract Downloaded Archive"
+cd /home/ubuntu
+rm -rf login
+unzip -o /tmp/login.zip &>>$LOG
+mv login-main login
+cd /home/ubuntu/login
+export GOPATH=/home/ubuntu/go
+export GOBIN=$GOPATH/bin
+go get
+go build
 Stat $?
 
-Head "Now, lets set up the service with systemctl."
-sed -i -e "s/user_endpoint/users.${DOMAIN}/" /home/app/login/systemd.service
+Head "Update EndPoints in Service File"
+sed -i -e "s/user_endpoint/users.${DOMAIN}/" /home/ubuntu/login/systemd.service
+Stat $?
