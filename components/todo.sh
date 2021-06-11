618b0127918bb1073c6c3367b8f6414829aa33fc
Srihari <Srihari@zelarsoft.com>
2021-06-11 22:29:33 +0530

    update

--- components/todo.sh
+++ components/todo.sh
@@ -28,3 +28,12 @@ Head "installing npm in todo"
 npm install -y &>>$LOG
 Stat $?
 
+Head "pass the EndPoints in Service File"
+sed -i -e "s/redis-endpoint/redis.${DOMAIN}/" systemd.service
+Stat $?
+
+Head "Setup the systemd Service"
+mv systemd.service /etc/systemd/system/todo.service &>>$LOG
+Stat $?
+systemctl daemon-reload && systemctl start todo && systemctl enable todo &>>$LOG
+Stat $?
